eec1c3fab04947c050dba1b6e57cf41022a834bc
Mailonga Albuquerque <81332475+Mailonga@users.noreply.github.com>
2022-05-01 16:00:08 -0300

    Update rotas.js

--- Cadastro De Produtos/Componentes/rotas.js
+++ Cadastro De Produtos/Componentes/rotas.js
@@ -1,18 +1,22 @@
 import React from "react";
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CadastroProdutos from "./views/cadastroProdutos.jsx";
+import Home from './component/Pages/Home';
+import Consulta from './component/Pages/Consulta';
 
-import Home from "./views/home";
 
 function Rotas(){
     return(
       <Router>
           <Switch>
-              <Route exact path = "/">
-               <Home/>
+              <Route exact path = "/Home">
+                  <Home/>
               </Route>
-              <Route exact = {true} path = "/cadastro-produtos">
+              <Route exact = {true} path = "/cadastroProdutos">
                   <CadastroProdutos/>
+              </Route>
+              <Route exact Path = "/Consulta">
+                  <Consulta/>
               </Route>
           </Switch>
       </Router>
