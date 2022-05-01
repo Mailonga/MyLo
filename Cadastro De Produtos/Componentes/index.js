e62ac3917ef1c68eb0e98a2536753ee883771dee
Mailonga Albuquerque <81332475+Mailonga@users.noreply.github.com>
2022-05-01 16:00:45 -0300

    Update index.js

--- Cadastro De Produtos/Componentes/index.js
+++ Cadastro De Produtos/Componentes/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 import './index.css';
 import 'bootswatch/dist/lux/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import Navbar from './component/Navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    < Navbar />
+    <Navbar/>
+    <App/>
   </React.StrictMode>
 );
